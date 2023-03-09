55cbd3d417a3709fab78b00d789a561c77acbb94
vrxmike <alvinvrxmike@icloud.com>
2023-03-09 05:26:51 -0800

    1-Task-Hotfix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_t(parent, value);
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
 
